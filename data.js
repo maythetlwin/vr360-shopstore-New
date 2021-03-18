6f7170bd8fca6a65cfe89ee530d19e2f5f312a08
KhaingThidaHtun <khaingthidahtun@ad.gicjp.com>
2021-03-18 12:57:31 +0900

    first commit

---
+++ data.js
@@ -0,0 +1,356 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "事務所奥側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.248697660214189,
+        "pitch": 0.14554086033489888,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.32561118881295137,
+          "pitch": 0.49479084242408433,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.8218382117713823,
+          "pitch": 0.25233288832404455,
+          "rotation": 7.0685834705770345,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.9586666178411747,
+          "pitch": 0.6292527787464444,
+          "rotation": 7.0685834705770345,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.5459086348449738,
+          "pitch": 0.8147685495570745,
+          "rotation": 16.493361431346422,
+          "target": "6-"
+        },
+        {
+          "yaw": 1.6984435372995685,
+          "pitch": 0.1243742211538823,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.575198850091848,
+          "pitch": 0.05253081219385791,
+          "title": "岩永智之さん",
+          "text": "<b>代表取締役社長</b><div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/iwanagasan.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": 2.734399581518547,
+          "pitch": 0.08922947791404567,
+          "title": "小西剛さん",
+          "text": "<b>取締役会</b><div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/konishisan.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": -1.671922143614383,
+          "pitch": 0.16719995533926202,
+          "title": "管理本部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/management.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": 1.3542015186730882,
+          "pitch": 0.06741855580304446,
+          "title": "海外戦略事業部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img class='card1' src='img/oversea.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "事務所",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.3213734743658385,
+        "pitch": -0.008471040315006562,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.3761006230202817,
+          "pitch": -0.02484798051116499,
+          "title": "竹迫誠さん",
+          "text": "<b>人事部</b><div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/takesakosan.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": -2.9519487815058163,
+          "pitch": 0.08382283720840178,
+          "title": "カゥン ミャット トゥンさん", 
+          "text": "<b>取締役会</b><div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/kaungmyattunsan.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": -2.2290998554554324,
+          "pitch": 0.04473256257989355,
+          "title": "<div style=\"font-family: &quot;Segoe UI&quot;, system-ui, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, sans-serif;\">松浦真人さん</div>",
+          "text": "<b>GSE事業部 海外戦略</b><div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": 1.9897887116277353,
+          "pitch": 0.034082368239930716,
+          "title": "小笠原享さん",
+          "text": "<b>取締役会</b><div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/ogasawarasan.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": 1.4004336787642693,
+          "pitch": 0.14240248270355416,
+          "title": "プロジェクト開発事業部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img src='img/projectdevelopment1.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        },
+        {
+          "yaw": 0.4946730278329543,
+          "pitch": 0.08856266539160274,
+          "title": "プロジェクト開発事業部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img class='card2' src='img/projectdevelopment2.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "人事部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.055638625475666004,
+        "pitch": -0.044596206224024826,
+        "fov": 0.9515799853531559
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5568270609499049,
+          "pitch": -0.05380836645822207,
+          "title": "人事部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img class='card1' src='img/humanresource.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "情報システム部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.05999014896932131,
+          "pitch": 0.1000237576567482,
+          "title": "情報システム部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img class='card3' src='img/information.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "海外戦略事業部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5975355250939947,
+        "pitch": -0.12837514904351188,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3814142592531677,
+          "pitch": 0.08425010397868604,
+          "title": "海外戦略事業部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img class='card2' src='img/oversea.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "プロジェクト開発事業部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.5269912268029202,
+        "pitch": 0.13282474001448463,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3411951030216613,
+          "pitch": 0.15952561601886472,
+          "title": "プロジェクト開発事業部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img class='card1' src='img/project.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "管理本部",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.2874879436084399,
+        "pitch": 0.2174709754078723,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.5511283205108768,
+          "pitch": 0.18451936733924512,
+          "title": "管理本部",
+          "text": "<div><b>グローバル イノベーション コンサルティング ㈱</b></div><br><div><img class='card1' src='img/hr.png' width='350' height='450'></div><div><button class ='button'><a href='https://en.gicjp.com/' target='_blank'>Learn More</a></button></div>"
+        }
+      ]
+    }
+  ],
+  "name": "GIC VR Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
